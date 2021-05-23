00c9f6d94ab86b029bcd45a79f6c80796e301084
harish-sethuraman <harish.sethuraman@freshworks.com>
2021-05-23 23:18:17 +0530

    :disappointed:  Adding prod config

--- configs/webpack.production.js
+++ configs/webpack.production.js
@@ -1,4 +1,16 @@
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 module.exports = () => ({
   mode: 'production',
-  plugins: [],
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'PORTFOLIO',
+      filename: 'remoteEntry.js',
+      remotes: {
+        PORTFOLIO: 'PORTFOLIO@http://strek.netlify.app/remoteEntry.js',
+        VSCODE: 'VSCODE@http://vsclone.netlify.app/remoteEntry.js',
+      },
+      shared: [{ react: { singleton: true } }],
+    }),
+  ],
 });
