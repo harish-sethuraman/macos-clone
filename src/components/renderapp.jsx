9c8e8bf8b36fc00cfb38de2198c9952cb5d13035
harish-sethuraman <harish.sethuraman@freshworks.com>
2021-05-29 21:25:13 +0530

    :hammer:  Added iconslink

--- src/components/renderapp.jsx
+++ src/components/renderapp.jsx
@@ -27,6 +27,8 @@ const renderApp = (app) => {
       let prodCss = `<!DOCTYPE html>
       <html>
       <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
       <link rel="stylesheet" type="text/css" href="https://strek.netlify.app/portfolio.css">
       </head><body><div></div></body></html>`;
       return (
